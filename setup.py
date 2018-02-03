eded455f28e7a838ce0a6885572bd68f48dbb2e9
Sebastien Andrivet <sebastien@advtools.com>
2018-02-03 17:13:56 +0100

    Bump version: 1.0.1-dev → 1.0.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ long_description = '%s\n%s' % (
 
 setup(
     name='advbumpversion',
-    version='1.0.1-dev',
+    version='1.0.1',
     url='https://github.com/andrivet/advbumpversion',
     packages=find_packages('.'),
     author='Sebastien Andrivet',
